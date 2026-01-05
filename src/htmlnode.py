73b2776a4654458ffdc37441b0fc343c943e910f
jdcampeau <jdcgithub@duck.com>
2026-01-05 16:03:06 -0600

    added htmlnode.py. Unit tests to come

---
+++ src/htmlnode.py
@@ -0,0 +1,22 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        form_str = ""
+        for key, value in self.props:
+            form_str = f'{form_str} {key}="{value}",'
+        return form_str
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
