56b494d4d4b9edfa921f2b2a51f7f4dddc302e8a
jdcampeau <jdcgithub@duck.com>
2026-01-12 14:31:23 -0600

    added unit test(s) for LeafNodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test__repr__(self):
@@ -14,3 +14,7 @@ class TestHTMLNode(unittest.TestCase):
         child2 = HTMLNode()
         node1 = HTMLNode("a", "boot.dev link", [child1, child2], {"href": "https://www.boot.dev/"})
         print(node1.props_to_html())
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
