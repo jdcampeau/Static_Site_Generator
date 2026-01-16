41c6e4945520c7f1c6cffa5570a628d26539b34b
jdcampeau <jdcgithub@duck.com>
2026-01-16 15:22:22 -0600

    added and tested textnode to htmlnode function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextType, TextNode, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -27,6 +27,12 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Test", TextType.LINK, "https://www.boot.dev/")
         node2 = TextNode("Test", TextType.LINK, "https://www.facebook.com/")
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import HTMLNode, LeafNode
+
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -25,3 +27,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(TextNode):
+    if TextNode.text_type == TextType.TEXT:
+        return LeafNode(None, TextNode.text)
+    if TextNode.text_type == TextType.BOLD:
+        return LeafNode("b", TextNode.text)
+    if TextNode.text_type == TextType.ITALIC:
+        return LeafNode("i", TextNode.text)
+    if TextNode.text_type == TextType.CODE:
+        return LeafNode("code", TextNode.text)
+    if TextNode.text_type == TextType.LINK:
+        return LeafNode("a", TextNode.text, {"href": TextNode.url})
+    if TextNode.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {TextNode.url: TextNode.text})
+    else:
+        raise Exception("Unknown text type")
+
+
