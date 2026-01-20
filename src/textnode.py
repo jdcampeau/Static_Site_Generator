8b9bd149a2bbb97acaf6c4fe8b6c06bac7db530a
jdcampeau <jdcgithub@duck.com>
2026-01-20 15:11:42 -0600

    started delimiter function, not tested yet

--- src/textnode.py
+++ src/textnode.py
@@ -28,6 +28,29 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+def split_nodes_delimiter(input_nodes, delimiter, text_type):
+    output = []
+    for node in input_nodes:
+        if node.text_type is not TextType.TEXT:
+            output.append(node)
+        else:
+            sub_output = []
+            string_list = node.text.split(delimiter)
+            if len(string_list) != 3:
+                raise Exception("invalid Markdown syntax")
+            for strng in string_list:
+                idx = 0
+                if idx == 1:
+                    new_node = TextNode(strng, text_type)
+                    sub_output.append(new_node)
+                    idx += 1
+                else:
+                    new_node = TextNode(strng, TextType.TEXT)
+                    sub_output.append(new_node)
+                    idx += 1
+            output.extend(sub_output)
+    return output
+
 def text_node_to_html_node(TextNode):
     if TextNode.text_type == TextType.TEXT:
         return LeafNode(None, TextNode.text)
