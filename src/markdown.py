4ffbee2f233f72ce0f5088ef0b0ca9ba0a890328
jdcampeau <jdcgithub@duck.com>
2026-02-24 15:50:11 -0600

    added rough block_to_block_type function. refactoring and testing next

--- src/markdown.py
+++ src/markdown.py
@@ -1,4 +1,60 @@
+from enum import Enum
+
 import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph" 
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list" 
+    ORDERED_LIST = "ordered list"
+
+def block_to_block_type(markdown_text): #input must be a single block, not a list of blocks or multiple blocks in a single string
+    if markdown_text.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    elif markdown_text.startswith("```\n") and markdown_text.startswith("```", -3):
+        return BlockType.CODE
+    elif markdown_text.startswith(">") or markdown_text.startwith("> "):
+        lines = True
+        for i in range(len(markdown_text)):
+            if markdown_text[i] == "\n":
+                if markdown_text[i+1] != ">" and markdown_text[i+1] != "> ":
+                    lines = False
+        if lines == False:
+            continue
+        else:
+            return BlockType.QUOTE
+    elif markdown_text.startswith("- "):
+        lines = True
+        for i in range(len(markdown_text)):
+            if markdown_text[i] == "\n":
+                if markdown_text[i+1] != "- ":
+                    lines = False
+        if lines == False:
+            continue
+        else:
+            return BlockType.UNORDERED_LIST
+    elif markdown_text.startswith("1. "):
+        lines = True
+        line = 2
+        for i in range(len(markdown_text)):
+            if markdown_text[i] == "\n":
+                if markdown_text[i+1] != "{line}":
+                    lines = False
+                else:
+                    line += 1
+                    if markdown_text[i+2] != ".":
+                        lines = False
+                    else:
+                        if markdown_text[i+3] != " ":
+                            lines = False
+        if line == False:
+            continue
+        else:
+            return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
 
 def extract_markdown_images(text):
     matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
